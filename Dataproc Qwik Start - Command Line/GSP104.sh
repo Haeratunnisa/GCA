a2c385a772cd73529622fb1943f6b7c2642343ee
Haeratunnisa <Hairatunnisa14@gmail.com>
2025-08-25 22:11:57 +0800

    Update GSP104.sh

--- Dataproc Qwik Start - Command Line/GSP104.sh
+++ Dataproc Qwik Start - Command Line/GSP104.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Google Cloud Dataproc Cluster and Job Submission - Complete Script
-# This script automates the creation of a Dataproc cluster and job submission
-
 # Color codes for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -12,7 +9,7 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 WHITE='\033[1;37m'
 BOLD='\033[1m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 # Function to print colored status messages
 print_status() {
